2f7abb67dc2b2c09ecc02b1d23fb928cf5fc561d
Baeist <lars18n2@stud.kea.dk>
2022-09-07 21:00:50 +0200

    delete update

--- src/main/java/com/example/restintro/RestIntroApplication.java
+++ src/main/java/com/example/restintro/RestIntroApplication.java
@@ -3,10 +3,13 @@ package com.example.restintro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Connection;
+
 @SpringBootApplication
 public class RestIntroApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(RestIntroApplication.class, args);
     }
 
